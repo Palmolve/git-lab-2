8236cd556127a4325d6eb2fb8b3d232206abe764
Sam Keller <sk209420@ohio.edu>
2022-09-07 17:58:44 -0400

    Here is the code

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+* File: git-lab-program.cc
+* Author: Sam Kelelr
+* Date: September 7, 2022
+* Description: This is the code for the second lab
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" <<endl;
+
+}
